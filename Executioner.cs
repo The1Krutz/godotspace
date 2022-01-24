9b71fbab1bd8f2c361534a791e258a5dfe2b9395
Thomas Kent <the1krutz@gmail.com>
2022-01-24 01:37:40 -0800

    GDS-3 start working on the arcade style spaceship tutorial
    imported assets, added the first script (and fixed the csproj file)

---
+++ Executioner.cs
@@ -0,0 +1,28 @@
+using Godot;
+
+/// <summary>
+/// template
+/// </summary>
+public class Executioner : KinematicBody {
+  // Signals
+
+  // Exports
+  [Export]
+  private float maxSpeed = 50.0f;
+  [Export]
+  private float acceleration = 0.6f;
+
+  // Public Fields
+
+  // Backing Fields
+
+  // Private Fields
+
+  // Constructor
+
+  // Lifecycle Hooks
+
+  // Public Functions
+
+  // Private Functions
+}
