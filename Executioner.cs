e05a9f5e7c744c887e9126a6281f857b12d4c468
Thomas Kent <the1krutz@gmail.com>
2022-01-25 21:57:23 -0800

    - add forward/backward - add lerpy camera - add test scene

--- Executioner.cs
+++ Executioner.cs
@@ -8,21 +8,36 @@ public class Executioner : KinematicBody {
 
   // Exports
   [Export]
-  private float maxSpeed = 50.0f;
+  public float MaxSpeed = 50.0f;
   [Export]
-  private float acceleration = 0.6f;
+  public float Acceleration = 0.6f;
 
   // Public Fields
 
   // Backing Fields
 
   // Private Fields
+  private Vector3 velocity = Vector3.Zero;
+  private float forwardSpeed;
 
   // Constructor
 
   // Lifecycle Hooks
+  public override void _PhysicsProcess(float delta) {
+    GetInput(delta);
+    velocity = Transform.basis.z * forwardSpeed;
+    MoveAndCollide(velocity * delta);
+  }
 
   // Public Functions
 
   // Private Functions
+  private void GetInput(float delta) {
+    if (Input.IsActionPressed("throttle_up")) {
+      forwardSpeed = Mathf.Lerp(forwardSpeed, MaxSpeed, Acceleration * delta);
+    }
+    if (Input.IsActionPressed("throttle_down")) {
+      forwardSpeed = Mathf.Lerp(forwardSpeed, 0, Acceleration * delta);
+    }
+  }
 }
