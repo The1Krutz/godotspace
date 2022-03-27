1b0d4c268dc172330b858fe0c60293bbe7484818
Thomas Kent <the1krutz@gmail.com>
2022-03-26 20:36:22 -0700

    update version

--- itch-upload.sh
+++ itch-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 release="prototype"
-version="0.01"
+version="0.02"
 
 butler push .export/Linux the1krutz/godotspace:linux-$release --userversion $version
 butler push .export/Windows the1krutz/godotspace:windows-$release --userversion $version
